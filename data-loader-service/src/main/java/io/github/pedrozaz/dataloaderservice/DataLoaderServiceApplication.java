0d21e893f638acb1e0ab4a23f7e14d23c94f2bed
Pedrozaz <guilhermebarb0sa@proton.me>
2025-11-10 13:44:32 -0300

    Initial commit

---
+++ data-loader-service/src/main/java/io/github/pedrozaz/dataloaderservice/DataLoaderServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.pedrozaz.dataloaderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLoaderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataLoaderServiceApplication.class, args);
+    }
+
+}
---
+++ data-loader-service/src/test/java/io/github/pedrozaz/dataloaderservice/DataLoaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pedrozaz.dataloaderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataLoaderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
